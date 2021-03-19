bd80cd53306b53e7a88da8644ecc7862bd9eb3cf
Mark <mark@glfusion.org>
2021-03-19 07:32:41 -0500

    Bump version

--- tag.php
+++ tag.php
@@ -31,7 +31,7 @@ $_TABLES['tag_menu']      = $_DB_table_prefix . 'tag_menu';
 
 $_TAG_CONF['pi_name']           = 'tag';
 $_TAG_CONF['pi_display_name']   = 'Tag';
-$_TAG_CONF['pi_version']        = '2.1.1';
+$_TAG_CONF['pi_version']        = '2.1.2';
 $_TAG_CONF['gl_version']        = '1.6.3';
 $_TAG_CONF['pi_url']            = 'https://www.glfusion.org/';
 ?>
